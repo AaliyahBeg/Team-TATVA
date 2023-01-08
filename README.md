b5abe34872737e0624839ab9c48e7be90cdcc37c
Aaliyah Beg <aaliyahbeg123@gmail.com>
2023-01-09 00:23:45 +0530

    Homepage pages thru widget

--- README.md
+++ README.md
@@ -4,12 +4,20 @@ An exciting project in hand to create a greener and cleaner world to live in.
 
 ## Getting Started
 
+Like to work elaborately on the problem related to the layers of destruction caused to the environment. 
+Tackling and providing solutions to all the environmental issues broadly classified into Land, Water, Air, Noise and Light Pollution.
+
 In this project we are having the following tracks :
-1) Land Pollution 
-2) Water Pollution
-3) Air Pollution
-4) Noise Pollution
-5) Light Pollution
+1) Land Pollution :  will track the solid waste generation and implement the concept of 4R effectively, Buying and selling of solid waste
+
+2) Water Pollution :  Promoting Rain Water harvesting techniques,  Ground Water Levels, pH Levels monitoring
+
+3) Air Pollution : AQI Trackers and Maps
+
+4) Noise Pollution : Noise Level Tracking through APIs
+
+5) Light Pollution :  Nocturnal birds are affected by the street lights in the night, thus we can monitor the use of lights in the night.
+
 
 ## Resources
 
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -23,27 +23,27 @@ class _HomeState extends State<Home> {
   List<CardItem> items =[
     CardItem(
       urlImage: 'https://4.bp.blogspot.com/-N_WdeyPGz10/TocPGEkD1YI/AAAAAAAAAGU/h8Abdw5Ukdw/s1600/Soil+pollution2.jpg',
-      title:"Land Pollution",
+      title:"LAND POLLUTION",
       desc: "",
     ),
     CardItem(
       urlImage:'https://th.bing.com/th/id/OIP.L2Q2NieXVo4DQbkHnNOTQgHaE8?pid=ImgDet&rs=1' ,
-      title:"Water Pollution",
+      title:"WATER POLLUTION",
       desc: "",
     ),
     CardItem(
       urlImage: 'https://hhp-blog.s3.amazonaws.com/2020/07/GettyImages-1098018152.jpg',
-      title:"Air Pollution",
+      title:"AIR POLLUTION",
       desc: "",
     ),
     CardItem(
       urlImage: 'https://buzznigeria.com/wp-content/uploads/2016/06/noise-pollution.jpg',
-      title:"Noise Pollution",
+      title:"NOISE POLLUTION",
       desc: "",
     ),
     CardItem(
       urlImage: 'https://th.bing.com/th/id/R.a9eb12ffaa1b41e866e0201c4ab8ec0e?rik=TdI6r%2b9Fv%2bT7Wg&riu=http%3a%2f%2f3.bp.blogspot.com%2f-LAxx2eW-LxM%2fVpugCSfkuxI%2fAAAAAAAABJQ%2fwA0P-9iSi9c%2fs1600%2fstreet-light.jpg&ehk=fuBrNPLvM5PWNh2PWc1SbIkYhclbBB%2fnofgJ%2fsM5tms%3d&risl=&pid=ImgRaw&r=0',
-      title:"Light Pollution",
+      title:"LIGHT POLLUTION",
       desc: "",
     ),
   ];
@@ -153,21 +153,39 @@ class _HomeState extends State<Home> {
           //     )
           //
           // ),
+
           Container(
-            height: 256,
-            child: ScrollSnapList(
-              padding: EdgeInsets.all(16),
-              scrollDirection: Axis.horizontal,
-                onItemFocus:(int){
+            color: Colors.grey[200],
+            child: Column(
+              children: [
+                SizedBox(height:20),
+                Text(
+                  "CONTROL",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
 
-                },
-                itemSize: 200,
-                dynamicItemSize: true,
-                // itemBuilder: (context,_)=> SizedBox(width:12),
-                itemBuilder: (context,index) => buildCard(item:items[index]),
-                itemCount: 7,
+                ),
+                Container(
+                  height: 256,
+                  child: ScrollSnapList(
+                    padding: EdgeInsets.fromLTRB(16,12,16,12),
+                    scrollDirection: Axis.horizontal,
+                      onItemFocus:(int){
+
+                      },
+                      itemSize: 200,
+                      dynamicItemSize: true,
+                      // itemBuilder: (context,_)=> SizedBox(width:12),
+                      itemBuilder: (context,index) => buildCard(item:items[index]),
+                      itemCount: 7,
+                  ),
+
+                ),
+              ],
             ),
-
           )
 
 
@@ -212,10 +230,74 @@ class _HomeState extends State<Home> {
             color: Colors.grey,
           ),
         ),
+       Container(
+
+         child:getWidget(item.title),
+
+       ),
 
 
       ],
     ),
   );
+  Widget getWidget(String title){
+    if(title=='LAND POLLUTION')
+      return Container(
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+
+          ],
+        ),
+    );
+    else if(title=='WATER POLLUTION')
+      return Container(
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+
+          ],
+        ),
+      );
+    else if(title=='AIR POLLUTION')
+      return Container(
+        child:Column(
+
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 20,),
+            Text(
+              "AQI GRAPHS",
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+              ),
+            ),
+
+          ],
+        ),
+      );
+    else if(title=='NOISE POLLUTION')
+      return Container(
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+
+          ],
+        ),
+      );
+    else if(title=='LIGHT POLLUTION')
+    return Container(
+      child:Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+
+        ],
+      ),
+    );
+    else return Container(
+
+      );
+  }
 }
 
