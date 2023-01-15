e7bf3926602064c16ab4a8cd657870a2910e914d
Aaliyah Beg <aaliyahbeg123@gmail.com>
2023-01-15 23:54:01 +0530

    Folder Structure updated

---
+++ lib/Land_Pollution/main_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Content of page 1'),
+    );
+  }
+}
